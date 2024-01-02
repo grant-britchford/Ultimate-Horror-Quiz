aecd274d5c9d2c578e8beab72fe886b2ae790df1
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-01-02 15:32:55 +0000

    changes to code for sript.js and style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,27 @@
-/* high score table */
-var highscoreList= [{score:1 name: highscoreLine[]},
-    {score:2 name: highscoreLine[]},
-    { score:3 name: highscoreLine[]},
-    { score:4 name: highscoreLine[] },
-    { score:5 name: highscoreLine[] },
+// high scores table
+let highScores= [
+    {'name:[] score:[]'}
 ];
-highscoreList.sort(function){
-    return score
+
+let table= document.createElement("table");
+
+let headerRow= document.createElement("tr");
+let nameHeader= document.createElement("th");
+nameHeader.textContent= "Name";
+let scoreHeader= document.createElement("th");
+scoreHeader.textContent= "Score";
+headerRow.appendChild(nameHeader);
+headerRow.appendChild(scoreHeader);
+table.appendChild(headerRow);
+
+for (let i=0; i<highScores.length; i++) {
+    let nameCell= document.createElement("tr");
+    nameCell.textContent= highScores[i].name;
+    let scoreCell= document.createElement("td");
+    scoreCell.textContent= highScores[i].score;
+    row.appendChild(nameCell);
+    row.apeendChild(scoreCell);
+    table.appendChild(row);
 }
 
-var table= document.createElement('table');
-var header= table.createTHead();
-var row= header.insertRow(0);
-var cell1= row.insertCell(0);
-var cell2= row.insertCell(1);
-cell1.innerHTML= '<b>Name</b>';
-cell2.innerHTML= '<b>Score</b>';
-for (var i=0; i<highscoreList.length; i++){
-    var row= table.insertRow(i+1);
-    var cell1= row.insertCell(0);
-    var cell2= row.insertCell(1);
-    cell1.innerHTML= highscoreList[i].Name;
-    cell2.innerHTML= highscoreList[i].Score;
-}
 document.body.appendChild(table);
