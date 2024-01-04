91ea8756be37d171b1583bbb98af85bb349c5dec
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-01-04 17:21:42 +0000

    debug for high score tableb html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,6 @@
 // high scores table
-let table= document.createElement("table");
-
-let headerRow= document.createElement("tr");
-let nameHeader= document.createElement("th");
-nameHeader.textContent= "Name";
-let scoreHeader= document.createElement("th");
-scoreHeader.textContent= "Score";
-headerRow.appendChild(nameHeader);
-headerRow.appendChild(scoreHeader);
-table.appendChild(headerRow);
-
-for (let i=0; i<highScores.length; i++) {
-    let nameCell= document.createElement("tr");
-    nameCell.textContent= highScores[i].name;
-    let scoreCell= document.createElement("td");
-    scoreCell.textContent= highScores[i].score;
-    row.appendChild(nameCell);
-    row.apeendChild(scoreCell);
-    table.appendChild(row);
+let table=('highScore');
+{
+    privateTransform.entryContainer;
+    private
 }
-
-document.body.appendChild(table);
