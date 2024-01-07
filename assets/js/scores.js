e5c7cea1a18e2221906e9585c4b32d95f235c416
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-01-07 16:58:00 +0000

    update to scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -6,9 +6,20 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
+    saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(15)
+
+    localStorage.setItem('highScores' JSON.stringify(highScores));
+    window.location.assign('/');
 };
