584d55206aee266ced011de2a750e2535e629a76
Grant Britchford <grant.britchford@hotmail.co.uk>
2024-01-16 13:19:03 +0000

    rebuild html and javascript code for buttons

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,31 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let buttons=document.getElementsByClassName("button");
-
-    for(let button of buttons){
-        button.addEventListeners('click', function(){
-            if(this.getAttribute('data-type') === 'submit'){
-                alert(`You Clicked Submit!`)
-            } else {
-                let gameType= this.getAttribute('data-type');
-                alert(`You Clicked ${gameType}`);
-            }
-        })
-    }
-})
-
-function runGame(){
-
+const button = btn;
+function btn(){
+document.getElementById("myButton").onclick;
 }
-function checkAnswer(){
-
-}
-function calucateCorrectAnswer(){
-
-}
-function incrementScore(){
-
-}
-function displayQuestion(){
-
-}
-
+console.log('btn');
