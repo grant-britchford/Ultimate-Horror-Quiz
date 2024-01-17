ff3df8b34158b16e83b881ad4d6895c046b5b7b1
Grant Britchford <grant.britchford@hotmail.co.uk>
2024-01-17 13:08:36 +0000

    debug alterations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,86 @@
-var myQuestions = [
+function displayquestions(quizContainer, question, answers){
+   let questions=[
    {
       question:["Which serial killer was the inspiration for Thomas Harris' Silence of the Lambs?"],
-      answers:[A,"Ed Gein", B,"Alfredo Balli Trevino", C,"Walter Ellis"],
-      correctAnswer: 'B' 
+      answers:['Ed Gein', "Alfredo Balli Trevino", "Walter Ellis"]
    },
    {
       question:["In 1992 who decapitated his wifes lover and presented her the head?"],
-      answers:[A,"Harold Shipman", B,"Roy Norris", C,"Stephen Schap"],
-      correctAnswer: 'C' 
+      answers:["Harold Shipman", "Roy Norris", "Stephen Schap"]
    },
    {
       question:["Who terrorised the Whitechapel district of London in 1888?"],
-      answers:[A,"Springheeled Jack", B,"Albert Fish", C,"Jack the Ripper"],
-      correctAnswer: 'C' 
+      answers:["Springheeled Jack", "Albert Fish", "Jack the Ripper"]
    },
    {
       question:["Which Australian serial killerwas inspired by Chucky the doll of the Childs Play movies?"],
-      answers:[A,"Johnathon Cruz", B,"Martin Bryant", C,"Edmund kemper"],
-      correctAnswer: 'B' 
+      answers:["Johnathon Cruz", "Martin Bryant", "Edmund kemper"]
    },
    {
       question:["Which serial killer in the 1970s killed because of a misplaced belief that warped his perception?"],
-      answers:[A,"Herbert Mullin", B,"Brenda Spencer", C,"ted bundy"],
-      correctAnswer: 'A' 
+      answers:["Herbert Mullin", "Brenda Spencer", "ted bundy"]
    },
    {
       question:["Jerry Brudos murdered womwn because of a fetish for which item?"],
-      answers:[A,"Shoes", B,"Underwear", C,"Dresses"],
-      correctAnswer: 'A' 
+      answers:["Shoes", "Underwear", "Dresses"]
    },
    {
       question:["Tracy Edwards escaped which serial killer which then led to his arrest?"],
-      answers:[A,"Ted Bundy", B,"Jeffery Dahmer", C,"Ed Gein"],
-      correctAnswer: 'B' 
+      answers:["Ted Bundy", "Jeffery Dahmer", "Ed Gein"]
    },
    {
       question:["Mark Branch was inspired by which movie killer?"],
-      answers:[A,"Freddy Krueger", B,"Michael Myers", C,"Jason Vorhees"],
-      correctAnswer: 'C' 
+      answers:["Freddy Krueger", "Michael Myers", "Jason Vorhees"]
    },
    {
       question:["The Canadian serial killer Gilbert Paul Jordan was also known as which name?"],
-      answers:[A,"Boozing Barber", B,"Sweeney Todd", C,"The butcher of Lyons"],
-      correctAnswer: 'A' 
+      answers:["Boozing Barber", "Sweeney Todd", "The butcher of Lyons"],
    },
    {
       question:["Who was the Toy Box killer?"],
-      answers:[A,"Nicolas Claux", B,"Charlie Brandt", C,"David Parker Ray"],
-      correctAnswer: 'C' 
+      answers:["Nicolas Claux", "Charlie Brandt", "David Parker Ray"]
    },
    {
       question:["The angel of death Josef Mengele died in which year?"],
-      answers:[A,"1959", B,"1979", C,"1949"],
-      correctAnswer: 'B' 
+      answers:["1959", "1979", "1949"] 
    },
    {
       question:["Dennis Rader aka the B.T.K killer worked as what type of technician?"],
-      answers:[A,"Computer technician", B,"Laboratory technician", C,"Alarm technician"],
-      correctAnswer: 'C' 
+      answers:["Computer technician", "Laboratory technician", "Alarm technician"] 
    },
    {
       question:["Gary Ridgway a.k.a The Green River killer served in which military branch?"],
-      answers:[A,"Airforce", B,"navy", C,"Army"],
-      correctAnswer: 'B' 
+      answers:["Airforce", "navy", "Army"]
    },
    {
       question:["Tsutomu Miyazaki blamed who for commanding him to kill?"],
-      answers:[A,"The Ratman", B,"The Dogman", C,"The Catman"],
-      correctAnswer: 'A' 
+      answers:["The Ratman", "The Dogman", "The Catman"]
    },
    {
       question:["Which country has the second most number of serial killers?"],
-      answers:[A,"Iceland", B,"The Philippines", C,"The United States"],
-      correctAnswer: 'B' 
+      answers:["Iceland", "The Philippines", "The United States"]
    },
    {
       question:["Norman bates of Psycho was based on which serial killer who had an obessive bond with his mother?"],
-      answers:[A,"Ted Bundy", B,"Ed Gein", C,"Peter Sutcliffe"],
-      correctAnswer: 'B' 
+      answers:["Ted Bundy", "Ed Gein", "Peter Sutcliffe"], 
    },
    {
       question:["Richard Delmer Boyer copied which horroe movie for his 1982 murders?"],
-      answers:[A,"The texas chainsaw massacre", B," Halloween 2", C,"Last house on the left"],
-      correctAnswer: 'B' 
+      answers:["The texas chainsaw massacre", " Halloween 2", "Last house on the left"],
    },
    {
       question:["Dana Sue Gray gave what reason for her 1994 murder spree?"],
-      answers:[A,"Retail therapy", B,"Voices in her head", C,"The excitement"],
-      correctAnswer: 'A' 
+      answers:["Retail therapy", "Voices in her head", "The excitement"]
    },
    {
       question:["Daniel LaPlante stalked the family he murdered by living in...?"],
-      answers:[A,"...Basement", B,"...Walls", C,"...Attic"],
-      correctAnswer: 'B' 
+      answers:["...Basement", "...Walls", "...Attic"]
    },
    {
       question:["Which unsolved case still haunts Finland?"],
-      answers:[A,"Lake Inari", B,"Lake Orivesi", C,"Lake Bodom"],
-      correctAnswer: 'C' 
+      answers:["Lake Inari", "Lake Orivesi", "Lake Bodom"]
    }
-];
+   ]};
+   
 
 
