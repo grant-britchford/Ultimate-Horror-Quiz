d345ab64e73b4d67f8937e509226fe8b3b8e16fe
Grant Britchford <grant.britchford@hotmail.co.uk>
2024-01-20 18:24:35 +0000

    changed javascript code to make it more easy to read

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,209 +1,155 @@
-document.addEventListener("DOMContentLoaded", function() {
-   let buttons= document.getElementsByTagName("button");
-   
-   for (button of buttons) {
-      button.addEventListener("click", function() {
-        if (this.getAttribute("data-type") === "submit") {
-         alert("You clicked Submit!")
-        } else {
-         let gameType= this.getAttribute("data-type");
-         runGame("quiz");
-        }
-      })
-   }
-})
-
-function runGame(gameType) {
-   let questions= myQtn.floor(myQtn.random() * 20);
-}
-
-function displayQuestion(questions) {
-   document.getElementById("myQtn").textContent;
-};
-
-let questions= [
+const quizArray = [
    {
       question:"Which serial killer was the inspiration for Thomas Harris' Silence of the Lambs?",
-      answers: {
-         A:"Ed Gein", 
-         B:"Alfredo Balli Trevino", 
-         C:"Walter Ellis"
-      },
-      correctAnswer: "B"
+      choices: ["Ed Gein", "Alfredo Balli Trevino", "Walter Ellis"],
+      answer: "Alfredo Balli Trevino"
    },
    {
       question:"In 1992 who decapitated his wifes lover and presented her the head?",
-      answers: {
-         A:"Harold Shipman", 
-         B:"Roy Norris", 
-         C:"Stephen Schap"
-      },
-      correctAnswer: "B"
+      choices: ["Harold Shipman", "Roy Norris", "Stephen Schap"],
+      answer: "Roy Norris"
    },
    {
       question:"Who terrorised the Whitechapel district of London in 1888?",
-      answers: {
-         A:"Springheeled Jack", 
-         B:"Albert Fish", 
-         C:"Jack the Ripper"
-      },
-      correctAnswer: "C"
+      choices: ["Springheeled Jack", "Albert Fish", "Albert Fish",],
+      answer: "Albert Fish"
    },
    {
       question:"Which Australian serial killerwas inspired by Chucky the doll of the Childs Play movies?",
-      answers: {
-         A:"Johnathon Cruz", 
-         B:"Martin Bryant", 
-         C:"Edmund kemper"
-      },
-      correctAnswer: "B"
+      choices: ["Johnathon Cruz", "Martin Bryant", "Edmund kemper"],
+      answer: "Martin Bryant"
    },
    {
       question:"Which serial killer in the 1970s killed because of a misplaced belief that warped his perception?",
-      answers: {
-         A:"Herbert Mullin", 
-         B:"Brenda Spencer", 
-         C:"ted bundy"
-      },
-      correctAnswer: "A"
+      choices: ["Herbert Mullin", "Brenda Spencer", "ted bundy"],
+      answer: "Herbert Mullin"
    },
    {
       question:"Jerry Brudos murdered womwn because of a fetish for which item?",
-      answers: {
-         A:"Shoes", 
-         B:"Underwear", 
-         C:"Dresses"
-      }, 
-      correctAnswer: "A"
+      choices: ["Shoes", "Underwear", "Dresses"],
+      answer: "Shoes"
    },
    {
       question:"Tracy Edwards escaped which serial killer which then led to his arrest?",
-      answers: {
-         A:"Ted Bundy", 
-         B:"Jeffery Dahmer", 
-         C:"Ed Gein"
-      }, 
-      correctAnswer: "B"
+      choices: ["Ted Bundy", "Jeffery Dahmer", "Ed Gein"],
+      answer: "Jeffery Dahmer"
    },
    {
       question:"Mark Branch was inspired by which movie killer?",
-      answers: {
-         A:"Freddy Krueger", 
-         B:"Michael Myers", 
-         C:"Jason Vorhees"
-      },
-      correctAnswer: "C"  
+      choices: ["Freddy Krueger", "Michael Myers", "Jason Vorhees"],
+      answer:  "Jason Vorhees"
    },
    {
       question:"The Canadian serial killer Gilbert Paul Jordan was also known as which name?",
-      answers: {
-         A:"Boozing Barber", 
-         B:"Sweeney Todd", 
-         C:"The butcher of Lyons"
-      },
-      correctAnswer: "A"
+      choices: ["Boozing Barber", "Sweeney Todd", "The butcher of Lyons"],
+      answer: "Boozing Barber"
    },
    {
       question:"Who was the Toy Box killer?",
-      answers: {
-         A:"Nicolas Claux", 
-         B:"Charlie Brandt", 
-         C:"David Parker Ray"
-      },
-      correctAnswer: "C"  
+      choices: ["Nicolas Claux", "Charlie Brandt", "David Parker Ray"],
+      answer: "David Parker Ray" 
    },
    {
       question:"The angel of death Josef Mengele died in which year?",
-      answers: {
-         A:"1959", 
-         B:"1979", 
-         C:"1949"
-      },
-      correctAnswer: "B"
+      choices: ["1959", "1979", "1949"],
+      answer: "1979"
    },
    {
       question:"Dennis Rader aka the B.T.K killer worked as what type of technician?",
-      answers: {
-         A:"Computer technician", 
-         B:"Laboratory technician", 
-         C:"Alarm technician"
-      },
-      correctAnswer: "C"  
+      choices: ["Computer technician", "Laboratory technician", "Alarm technician"],
+      answer: "Alarm technician"
    },
    {
       question:"Gary Ridgway a.k.a The Green River killer served in which military branch?",
-      answers: {
-         A:"Airforce", 
-         B:"navy", 
-         C:"Army"
-      },
-      correctAnswer: "B"
+      choices: ["Airforce", "navy", "Army"],
+      answer: "navy"
    },
    {
       question:"Tsutomu Miyazaki blamed who for commanding him to kill?",
-      answers: {
-         A:"The Rat man", 
-         B:"The Dog man", 
-         C:"The Cat man"
-      },
-      correctAnswer: "A"
+      choices: ["The Rat man", "The Dog man", "The Cat man"],
+      answer: "The Rat man"
    },
    {
       question:"Which country has the second most number of serial killers?",
-      answers: {
-         A:"Iceland", 
-         B:"The Philippines", 
-         C:"The United States"
-      },
-      correctAnswer: "B"
+      choices: ["Iceland", "The Philippines", "The United States"],
+      answer: "The Philippines"
    },
    {
       question:"Norman bates of Psycho was based on which serial killer who had an obessive bond with his mother?",
-      answers: {
-         A:"Ted Bundy", 
-         B:"Ed Gein", 
-         C:"Peter Sutcliffe"
-      },
-      correctAnswer: "B"
+      choices: ["Ted Bundy", "Ed Gein", "Peter Sutcliffe"],
+      answer: "Ed Gein"
    },
    {
       question:"Richard Delmer Boyer copied which horroe movie for his 1982 murders?",
-      answers: {
-         A:"The texas chainsaw massacre", 
-         B:" Halloween 2", 
-         C:"Last house on the left"
-      },
-      correctAnswer: "B"
+      choices: ["The texas chainsaw massacre", "Halloween 2", "Last house on the left"],
+      answer: "Halloween 2"
    },
    {
       question:"Dana Sue Gray gave what reason for her 1994 murder spree?",
-      answers: {
-         A:"Retail therapy", 
-         B:"Voices in her head", 
-         C:"The excitement"
-      },
-      correctAnswer: "A"
+      choices: ["Retail therapy", "Voices in her head", "The excitement"],
+      answer: "Retail therapy"
    },
    {
       question:"Daniel LaPlante stalked the family he murdered by living in...?",
-      answers: {
-         A:"...Basement", 
-         B:"...Walls", 
-         C:"...Attic"
-      },
-      correctAnswer: "B"
+      choices: ["...Basement", "...Walls", "...Attic"],
+      answer: "...Walls",
    },
    {
       question:"Which unsolved case still haunts Finland?",
-      answers: {
-         A:"Lake Inari", 
-         B:"Lake Orivesi", 
-         C:"Lake Bodom"
-      },
-      correctAnswer: "C"  
+      choices: ["Lake Inari", "Lake Orivesi", "Lake Bodom"],
+      answer: "Lake Bodom" 
    }
 ];
-console.log(questions);
+
+const question = document.getElementById("question");
+const answers = document.getElementById("choices");
+const submitButton = document.getElementById("submit");
+
+let currentQuestion = 0;
+let score = 0;
+let correctAnswer = 3;
+
+function showQuestion() {
+   const quiz = quizArray[currentQuestion];
+   question.innerHTML = question.quizArray;
+
+   choices.innerHTML = "";
+   question.choices.forEach(option => {
+      const button = document.createElement("button");
+      button.innerText = choices;
+      choices.appendChild(button);
+      button.addEventListener("click", selectAnswer);
+   })
+};
+
+function selectAnswer(e) {
+   const selectedbutton = e.target;
+   const choices = quiz[currentQuestion].answer;
+
+   if (selectedbutton.innertext === answers) {
+      score ++;
+   } 
+
+   currentQuestion ++;
+
+   if (currentQuestion < quiz.length) {
+      showQuestion();
+   } else {
+      showResult();
+   }
+}
+
+function showResult() {
+   quiz.innerHTML = `<h3>Congratulations you have completed the quiz</h3>
+   <p>You Scored: ${score} / ${quiz.length}</p>`;
+}
+
+showQuestion();
+
+
+
+
+
 
    
   
